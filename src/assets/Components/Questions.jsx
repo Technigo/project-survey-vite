4405529e2f6e313a40c3c616af3f5ca0da4a7712
Hang-Nguyen-Vu <nguyenvuhang@gmail.com>
2023-09-25 14:55:39 +0200

    add question on fitness goal

--- src/assets/Components/Questions.jsx
+++ src/assets/Components/Questions.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { Summary } from "./Summary";
 
 export const Questions = () => {
     return (
       <>
         {/* Add questions here */}
+        <label>
+          What is your fitness goal?
+          <textarea name="fitness-goal" rows={4} cols={40} placeholder="I aim to..." />
+        </label>
         <Summary />
       </>
       
