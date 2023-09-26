5cbe9ccf57ea41f6f2badeb274c1d393a0a8d88c
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-09-26 08:30:40 +0200

    corrected names and added import!

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,20 @@
-
-
+/* eslint-disable react/prop-types */
+import { FavoritePerson } from "./FavoritePerson.jsx"
+import {  Celebrity } from "./Celebrity.jsx"
+import { SelectPerson } from "./SelectPerson.jsx"
+import { Activity } from "./Activity.jsx"
+import { Name } from "./Name.jsx"
+import { Country } from "./Country.jsx"
 export const Summary = () => {
   return (
-    <div>Summary</div>
+    <>
+      <FavoritePerson />
+      <Celebrity />
+      <SelectPerson />
+      <Activity />
+      <Name />
+      <Country />
+      <div>Summary</div>
+      </>
   )
 }
