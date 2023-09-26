66b8dbb5863695f67988c8c706708f011c64458a
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-26 20:02:26 +0200

    Added card & container components with css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import Card from "./components/Card";
+import Container from "./components/Container";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <Container>
+      <Card>
+        <h1>Welcome to our mushroom survey!</h1>
+        <p>In this survey we would like you to answer a couple of questions regarding mushrooms.</p>
+        <img src="/mushroom.jpg"></img>
+      </Card>
+      <Card>
+        <h1></h1>
+      </Card>
+      <Card>
+        <h1></h1>
+      </Card>
+      <Card>
+        <h1></h1>
+      </Card>
+    </Container>
+  );
 };
