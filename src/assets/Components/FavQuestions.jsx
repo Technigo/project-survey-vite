70369ea71aa5e5c147ed7297d6a2f8fd130ebdc2
Minadarabi <minaadabirii@gmail.com>
2023-09-26 13:29:31 -0700

    change the radio to checkbox

--- src/assets/Components/FavQuestions.jsx
+++ src/assets/Components/FavQuestions.jsx
@@ -13,28 +13,28 @@ export const FavQuestions = () => {
   return (
     
                    
-        <div className="radio-questions">
+        <div className="checkbox-questions">
             
             <label htmlFor="">What is your favourite exercise?</label>
            
            <label htmlFor="walking">Walking
-           <input type="radio" name="walking" id="walking" value={"Walking"} checked={favourite === "Walking"} onChange={onRadioChange} /> 
+           <input type="checkbox" name="walking" id="walking" value={"Walking"} checked={favourite === "Walking"} onChange={onRadioChange} /> 
              
            </label>
            <label htmlFor="running">Running
-           <input type="radio" name="running" id="running" value={"Running"} checked={favourite === "Running"} onChange={onRadioChange} />
+           <input type="checkbox" name="running" id="running" value={"Running"} checked={favourite === "Running"} onChange={onRadioChange} />
              
            </label>
            <label htmlFor="gym"> Going to GYM
-           <input type="radio" name="gym" id="gym" value={"Going to GYM"} checked={favourite === "Going to GYM"} onChange={onRadioChange} />
+           <input type="checkbox" name="gym" id="gym" value={"Going to GYM"} checked={favourite === "Going to GYM"} onChange={onRadioChange} />
              
            </label>
            <label htmlFor="swimming">Swimming
-           <input type="radio" name="swimming" id="swimming" value={"Swimming"} checked={favourite === "Swimming"} onChange={onRadioChange} /> 
+           <input type="checkbox" name="swimming" id="swimming" value={"Swimming"} checked={favourite === "Swimming"} onChange={onRadioChange} /> 
              
            </label>
            <label htmlFor="other">Other
-           <input type="radio" name="other" id="other" value={"Others"} checked={favourite === "Others"} onChange={onRadioChange} /> 
+           <input type="checkbox" name="other" id="other" value={"Others"} checked={favourite === "Others"} onChange={onRadioChange} /> 
 
            </label>
                
