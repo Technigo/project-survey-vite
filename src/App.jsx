9a25b82e81fd5bfb9e452f4386983b50b581bc1a
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-27 22:35:38 +0200

    Made a form database

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,92 @@ import Card from "./components/Card";
 import Container from "./components/Container";
 
 export const App = () => {
-  const [step, setStep] = useState('form');
+    // This will act as "database" for our form where we store our values to pass them to the components.
+    const [form, setForm] = useState({
+        name: "",
+        yearsPickingMushrooms: "",
+        favoriteMushroom: "",
+        prepareMushroom: ""
+    });
 
-  const goToSummary = () => setStep('summary');
+    // Sets a value in our form based on the name property of our target input.
+    const setFormValue = (event) => {
+        const { name, value } = event.target;
+        setForm({
+            ...form,
+            [name]: value,
+        });
+    }
 
-  return (
-    <Container>
-      {step === 'form' && (
-        <>
-          <Card>
-            <h1>Welcome to our mushroom survey!</h1>
-            <p>In this survey we would like you to answer a couple of questions regarding mushrooms.</p>
-            <img src="/mushroom.jpg"></img>
-            <input />
-            <input />
-          </Card>
+    console.log(form)
 
-          <Card>
-            <p></p>
-            <input name="test" type="radio" />
-            <input name="test" type="radio" />
-            <input name="test" type="radio" />
-          </Card>
+    const [step, setStep] = useState('form');
 
-          <Card>
-            <p></p>
-            <select>
-              <option>1</option>
-              <option>2</option>
-            </select>
-          </Card>
+    const goToSummary = () => setStep('summary');
 
-          <Card>
-            <p></p>
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
-          </Card>
+    return (
+        <Container>
+            {step === 'form' && (
+                <>
+                    <Card>
+                        <h1>Welcome to our mushroom survey!</h1>
+                        <p>In this survey we would like you to answer a couple of questions regarding mushrooms.</p>
+                        <img src="/mushroom.jpg"></img>
+                        <input
+                            name="name"
+                            value={form.name}
+                            onChange={setFormValue}
+                        />
+                    </Card>
 
-          <button onClick={goToSummary}>Submit</button>
-        </>
-      )}
-      {step === 'summary' && (
-        <p>Hej</p>
-      )}
-    </Container>
-  );
+                    <Card>
+                        <p>How many years have you been picking mushrooms?</p>
+                        <input
+                            name="yearsPickingMushrooms"
+                            type="radio"
+                            checked={form.yearsPickingMushrooms === "0-5"}
+                            value="0-5"
+                            onChange={setFormValue}
+                        />
+
+                        <input
+                            name="yearsPickingMushrooms"
+                            type="radio"
+                            checked={form.yearsPickingMushrooms === "5-10"}
+                            value="5-10"
+                            onChange={setFormValue}
+                        />
+
+                        <input
+                            name="yearsPickingMushrooms"
+                            type="radio"
+                            checked={form.yearsPickingMushrooms === "10+"}
+                            value="10+"
+                            onChange={setFormValue}
+                        />
+                    </Card>
+
+                    <Card>
+                        <p></p>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                    </Card>
+
+                    <Card>
+                        <p></p>
+                        <input type="checkbox" />
+                        <input type="checkbox" />
+                        <input type="checkbox" />
+                    </Card>
+
+                    <button onClick={goToSummary}>Submit</button>
+                </>
+            )}
+            {step === 'summary' && (
+                <p>{form.name}</p>
+            )}
+        </Container>
+    );
 };
