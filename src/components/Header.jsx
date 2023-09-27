6bf70ade3b58422e2f8e2d884dc9af6d087d2e84
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-09-27 13:56:46 +0200

    test to merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div>
             <img src="/Friends_logo.svg.png" alt="Friends" style={{ width: '50%', height: 'auto' }} />
-
+            <h1>Skriver ba här för o se om vi kan merga ihop sen</h1>
         </div>
     );
 };
