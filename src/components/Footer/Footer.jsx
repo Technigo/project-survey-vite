7dd3e296f0ab224659b03bc73d84ba855f5dcef2
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-09-27 16:42:19 +0200

    first steps of styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,6 @@ import './footer.css'
 
 export const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">Technigo project by: Emmy Dieden and Idah Collin</div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,6 @@ import './header.css'
 
 export const Header = () => {
   return (
-    <div className="header">Header</div>
+    <div className="header"><h3>Welcome to the Svenska turistföreningen!</h3> Please answere our survey to make us able to plan for the best experience. What kind of adventure are you up for summer of 2024?</div>
   )
 }
