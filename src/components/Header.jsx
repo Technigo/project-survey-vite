a4e28a89ee0c41a56755a8db5e5fc82be643d0e7
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-09-27 13:35:35 +0200

    added headercomponent and a picture

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class="img">
+            <img src="/Friends_logo.svg.png" alt="Friends" />
+
+        </div>
+    );
+};
+
+export default Header;
