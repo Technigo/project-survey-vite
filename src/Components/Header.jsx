dcd55091dc489ab7d23e4d5fac65cd9b96f8f4b8
Hang-Nguyen-Vu <nguyenvuhang@gmail.com>
2023-09-27 23:32:17 +0200

    try new background image

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
     <div className="form-container">
-      <h1 className="header-text">Wellness Survey</h1>
-      <p>Tell us more about your wellness activities and goal!</p>       
+      <h1 className="header-text">Fitness Survey</h1>
+      <p>Tell us more about your fitness activities and goal!</p>       
     </div>
   )
 }
