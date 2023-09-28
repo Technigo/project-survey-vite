c48b26e3b750561a1b2150b8364805825d50985f
Sheryll-Ann <shey.pc@gmail.com>
2023-09-28 16:47:44 +0200

    Styled storyboard and inner spans

--- src/components/CreateStoryBoard.jsx
+++ src/components/CreateStoryBoard.jsx
@@ -11,13 +11,13 @@ export const CreateStoryBoard = ({ formData }) => {
         <container className="story-page">
             <h1>{formData.name} and the Enchanted Pumpkin Patch</h1>
 
-            <p>In a {formData.townAdjective} town nestled among autumn hills, a fluffy cat named {formData.name} had a taste for adventure. Fall arrived, and with it came tales of the Enchanted Pumpkin Patch, whispered secrets among the elder cats.</p>
+            <p>In a <span className="town">{formData.townAdjective}</span> town nestled among autumn hills, a fluffy cat named <span className="name">{formData.name}</span> had a taste for adventure. Fall arrived, and with it came tales of the Enchanted Pumpkin Patch, whispered secrets among the elder cats.</p>
 
-            <p>One moonlit night, {formData.name} set out to find it. Guided by the full moon, the cat wandered through rustling leaves and {formData.journeyMood}. Eventually, it stumbled upon a clearing bathed in moonlight, filled with enormous, {formData.pumpkinAdjective} pumpkins.</p>
+            <p>One moonlit night, <span className="name">{formData.name}</span> set out to find it. Guided by the full moon, the cat wandered through rustling leaves and <span className="journey-mood">{formData.journeyMood}</span>. Eventually, it stumbled upon a clearing bathed in moonlight, filled with enormous, <span className="pumpkin-adjective">{formData.pumpkinAdjective}</span> pumpkins.</p>
 
-            <p>{formData.name} touched the largest pumpkin, and it transformed into {formData.pumpkinPatchCharacter}. "I can grant one wish," it said. {formData.name} wished for {formData.wishChoice} and laughter for the town's cats. {formData.pumpkinPatchCharacter.charAt(0).toUpperCase() + formData.pumpkinPatchCharacter.slice(1)} granted {formData.name}'s wish, and the patch came alive with happiness.</p>
+            <p><span className="name">{formData.name}</span> touched the largest pumpkin, and it transformed into <span className="mysterious-character">{formData.pumpkinPatchCharacter}</span>. "I can grant one wish," it said. <span className="name">{formData.name}</span> wished for <span className="wish">{formData.wishChoice}</span> and laughter for the town's cats. <span className="mysterious-character">{formData.pumpkinPatchCharacter.charAt(0).toUpperCase() + formData.pumpkinPatchCharacter.slice(1)}</span> granted <span className="name">{formData.name}</span>'s wish, and the patch came alive with happiness.</p>
 
-            <p>Cats from all around visited, and content purring filled the air. Watching from home, {formData.name} realized the essence of fall's magic—bringing joy to others. The Enchanted Pumpkin Patch became a cherished tradition, a tale told for generations from cat to kitten.</p>
+            <p>Cats from all around visited, and content purring filled the air. Watching from home, <span className="name">{formData.name}</span> realized the essence of fall's magic—bringing joy to others. The Enchanted Pumpkin Patch became a cherished tradition, a tale told for generations from cat to kitten.</p>
         </container>
 
     return (storyData)
