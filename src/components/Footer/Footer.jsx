ebf6ed8f2ae8899fb1c7add439daa687c1430590
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-09-28 22:24:17 +0200

    added styling to radio container

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,6 @@ import './footer.css'
 
 export const Footer = () => {
   return (
-    <div className="footer">Technigo project by: Emmy Dieden and Idah Collin</div>
+    <div className="footer">Technigo project by: <br></br>Emmy Dieden and Idah Collin</div>
   )
 }
