a4b15691d80c1aef35b804b3d39d8e6d96b90a3b
Carolina Luna <kroluna@gmail.com>
2023-09-29 16:04:51 +0200

    trying to style images

--- src/component/AgeQuestion.jsx
+++ src/component/AgeQuestion.jsx
@@ -12,7 +12,7 @@ export const AgeQuestion = ({ value, setValue, setIsAgeValid }) => {
 
   return (
     <label className="question-name-age">
-      What's your age?
+      What&apos;s your age?
       <input
         placeholder="Age here..."
         type="number"
@@ -22,44 +22,3 @@ export const AgeQuestion = ({ value, setValue, setIsAgeValid }) => {
     </label>
   );
 };
-
-// import { useState } from "react";
-
-// export const AgeQuestion = ({ value, setValue }) => {
-//   const [age, setAge] = [value, setValue];
-//   const [invalidAge, setInvalidAge] = useState(false);
-
-//   const ageInput = (e) => {
-//     const enteredAge = e.target.value;
-//     setAge(enteredAge);
-
-//     // Check if age is valid
-//     if (parseInt(enteredAge, 10) < 18) {
-//       setInvalidAge(true);
-//     } else {
-//       setInvalidAge(false);
-//       setValue("age", enteredAge);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {invalidAge ? (
-//         <div>
-//           <p>You must be older than 18 to take this survey.</p>
-//           {/* You can add a button or link to restart the survey here if you want */}
-//         </div>
-//       ) : (
-//         <label className="question-name-age">
-//           What&apos;s your age?
-//           <input
-//             placeholder="Age here..."
-//             type="number"
-//             onChange={ageInput}
-//             value={age}
-//           />
-//         </label>
-//       )}
-//     </div>
-//   );
-// };
--- src/component/DrinkSuggestion.jsx
+++ src/component/DrinkSuggestion.jsx
@@ -1,14 +1,17 @@
 import { DataOfDrinks } from "./DataOfDrinks";
 
 export const DrinkSuggestion = () => {
-    let drinkSuggestionObject = DataOfDrinks[0]
+  let drinkSuggestionObject = DataOfDrinks[0];
 
-    return (
-        <>
-            <div className="the-drink">
-                <img className="the-drink-pic" src={drinkSuggestionObject.drinkPicURL} />
-                <h2>{drinkSuggestionObject.name.toUpperCase()}</h2>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="the-drink">
+        <img
+          className="the-drink-pic"
+          src={drinkSuggestionObject.drinkPicURL}
+        />
+        <h2>{drinkSuggestionObject.name.toUpperCase()}</h2>
+      </div>
+    </>
+  );
+};
