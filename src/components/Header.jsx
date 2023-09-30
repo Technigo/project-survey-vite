a2c965f8cc5b7ce657f449bfc05d783d497f0572
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-30 22:10:35 +0200

    added "alt" to img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import styles from "./Header.module.css";
 const Header = () => {
     return (
         <div className={styles.header}>
-            <img src="/mushroom.jpg" />
+            <img src="/mushroom.jpg" alt="Image of a mushroom" />
             <div className={styles.headerContent}>
                 <h4>
                     In this survey we would like you to answer a couple of questions
