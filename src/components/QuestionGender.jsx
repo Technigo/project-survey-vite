4ea835d28ee549e0d32b5a46b0688170193cd525
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-09-30 10:01:36 +0200

    -m added message to QuestionGender

--- src/components/QuestionGender.jsx
+++ src/components/QuestionGender.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
 function QuestionGender({ selectedGender, onGenderChange }) {
   const genderOptions = ['Mrs Clause', 'Santa Clause', 'Other', 'Elf', 'Reindeer'];
 
   const handleGenderChange = (event) => {
     const selectedGenderOption = event.target.value;
     onGenderChange(selectedGenderOption);
+  };
+
+  // Define a function to get the appropriate message based on the selected gender
+  const getGenderMessage = (selectedGender) => {
+    switch (selectedGender) {
+      case 'Mrs Clause':
+        return 'You selected Mrs. Clause - The one who runs the show!';
+      case 'Santa Clause':
+        return 'You selected Santa Clause - Ho ho ho!';
+      case 'Other':
+        return 'You selected Other - Magical!';
+      case 'Elf':
+        return 'You selected Elf - Do you know Buddy?';
+      case 'Reindeer':
+        return 'You selected Reindeer - Flying high!';
+      default:
+        return '';
+    }
   };
 
   return (
@@ -24,7 +40,7 @@ function QuestionGender({ selectedGender, onGenderChange }) {
       </select>
       
       {/* Conditionally display the selected gender if it's not empty */}
-      {selectedGender && <p>You selected: {selectedGender}</p>}
+      {selectedGender && <p>{getGenderMessage(selectedGender)}</p>}
     </div>
   );
 }
