8160847fbed4b64a2192b892cd9ad2bf8ab89ccd
FridafridaL <fridus.lindskog@gmail.com>
2023-09-30 13:17:06 +0200

    test

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -6,8 +6,8 @@ export const Confirmation = ({ name, age, bloodType, allergies }) => {
         You are the intrepid explorer who is secured a golden ticket to the Red
         Planet in the agespan of {age}! With {bloodType} coursing through your
         veins, just watch out for those sneaky space allergens – {allergies}{" "}
-        might be a problem up there, but remember Earth allergies doesnt always have an effect in Mars
-
+        might be a problem up there, but remember Earth allergies doesnt always
+        have an effect in Mars
         <p>You are a Hero!</p>
       </p>
     </div>
