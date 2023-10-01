e2458b9c988f4fc214c6135df12375ab2031c968
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-10-01 12:09:50 +0200

    -m

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App.jsx"; // Update the import path for App component
-import "./index.css"; // Update the import path for CSS
+import App from "./App"; 
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
