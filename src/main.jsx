3a0b2f7d89b3b1e49ec768e343098b076b3b8819
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-10-01 11:33:38 +0200

    -m

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App"; // Update the import path for App component
+import App from "./App.jsx"; // Update the import path for App component
 import "./index.css"; // Update the import path for CSS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
