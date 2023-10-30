b9accf535475f1c1c0e43fcc0ccceabefd181e2c
BeckieMorton <beckam11@gmail.com>
2023-10-30 17:52:05 +0100

    responsiveness for food page fixed as per review and requirements

--- src/components/FavFood.jsx
+++ src/components/FavFood.jsx
@@ -1,22 +1,28 @@
-
 export const FavFood = ({ updateFormData, value }) => {
+  const favFood = (event) => updateFormData("favFood", event.target.value);
 
-    const favFood = (event) => updateFormData("favFood", event.target.value);
-
-    return (
-        <div className="food">
-
-            <div className="food-img">
-                <img src="/assets/pizza.png" alt="Pizza"></img>
-                <img src="/assets/salad.png"></img>
-                <img src="/assets/spaghetti.png"></img>
-                <img src="/assets/vegetables.png"></img></div>
-            <div className="foods-txt">
-                <label>What is your favourite food?</label>
-                <input type="text"
-                    value={value}
-                    onChange={favFood} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="food-container">
+      <div className="food-img">
+        <ul className="food-list">
+          <li>
+            <img src="/assets/pizza.png" alt="Pizza" />
+          </li>
+          <li>
+            <img src="/assets/salad.png" />
+          </li>
+          <li>
+            <img className="disappear-inmobile" src="/assets/spaghetti.png" />
+          </li>
+          <li>
+            <img className="disappear-inmobile" src="/assets/vegetables.png" />
+          </li>
+        </ul>
+      </div>
+      <div className="foods-txt">
+        <label>What is your favourite food?</label>
+        <input type="text" value={value} onChange={favFood} />
+      </div>
+    </div>
+  );
+};
