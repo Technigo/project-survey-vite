04313d2fcac59f6994dca2d6aaf00c04df08926e
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-10-31 19:37:41 +0100

    Moved MultiStep folder from assets to components

---
+++ src/components/MultiStep/TravelDestinationInput.jsx
@@ -0,0 +1,15 @@
+export const TravelDestinationInput = ({ value, updateDestination }) => {
+  const handleDestinationChange = (e) => updateDestination(e.target.value);
+
+  return (
+    <div>
+      <label htmlFor="travelDestination">Travel Destination:</label>
+      <input
+        type="text"
+        id="travelDestination"
+        value={value}
+        onChange={handleDestinationChange}
+      />
+    </div>
+  );
+};
