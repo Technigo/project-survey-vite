1cc5d737ffabb1e94ac6414344511034d2885ce4
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-10 00:05:48 +0100

    update [function] Q2 input type:color

--- src/Components/PageSection/InputTypes/ColorType.jsx
+++ src/Components/PageSection/InputTypes/ColorType.jsx
@@ -1,7 +1,24 @@
-export const ColorType = () => {
+export const ColorType = ({question, index,setOptionSummary}) => {
+
+    const handleChange = (event)=>{
+        const value = event.target.value;
+        console.log('Index:', index); 
+        console.log('Selected Option:', value); 
+        setOptionSummary(value);
+    }
+
     return (
-        <>
-        <p>Here is a color type input</p>
-        </>
+        
+         <section className={`input-section ${question.id}`}>
+            <label htmlFor={question.id}>{question.questionText}</label>
+                <input 
+                    type="color" 
+                    id={question.id} 
+                    name={question.id} 
+                    onChange={handleChange}
+                />
+              
+        </section>
+        
     )
 }
