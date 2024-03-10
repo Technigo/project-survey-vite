efed13a1548b8ea93c268ba0cd8b2e34e9fa22b8
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-10 20:35:22 +0100

    add [function] checkboxType

---
+++ src/Components/PageSection/InputTypes/CheckboxType.jsx
@@ -0,0 +1,32 @@
+export const CheckboxType = ({question, handleChecked, handleUncheck})=>{
+    
+    return (
+        <fieldset className={`input-section ${question.id}`}>
+            <legend >{question.questionText}</legend>
+                 <div className="option-secton">
+            {
+                question.options.map((option)=>(
+                    <div key={option.id} className="each-option">
+                        <input 
+                        type="checkbox" 
+                        id={option.id} 
+                        name={option.id} 
+                        value={option.optionText}
+                        onChange={(event)=>{
+                            if(event.target.checked){
+                                handleChecked(option.optionText);
+                            }else{
+                                handleUncheck(option.optionText);
+                            }
+                        }}
+                        />
+                        <label htmlFor="{option.id}">{option.optionText}</label>
+                    </div>
+                ))
+
+            }
+                
+                 </div>
+        </fieldset>
+    )
+}
