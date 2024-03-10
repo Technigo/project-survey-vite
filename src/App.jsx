d91e0b30240c0d17fe8ba38ac5999a16746ddf9e
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-03-10 23:02:07 +0100

    added functionality to hide form on send, and refresh form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
 import { useState } from "react";
 import ReactDOM from 'react-dom/client';
+import "./app.css";
 
+
+let toggle = "visible"
 export const App = () => {
 	//dropdown form
 	const [dropdown, setDropdown] = useState("");
 	const [name, setName] = useState("");
 	const [check, setChecked] = useState("");
 	const [value, setRadio] = useState("");
+	const [toggle, setClear] = useState("visible");
+
+
 	const handleChange = (e) => {
 		//prevent reload
 		e.preventDefault();
@@ -19,82 +25,93 @@ export const App = () => {
 		console.log(formJson);
 	}
 
+	const toggleVisible = () => {
+		if (toggle === "visible") {
+			setClear("hidden");
+		}
+		else if (toggle === "hidden") {
+			setClear("visible")
+
+		}
+	}
+
+	function refreshPage() {
+		window.location.reload(false);
+	}
+
 
 	return (
-		<div>
-
-
-			<form method="post" onSubmit={handleChange}>
-				<label>
-					Enter your name:
-					<input
-					value= {name}
-						name="username"
-
-						onChange={e => setName(e.target.value)}
-					/>
-				</label>
-				{setName !== "" && <p>Your name is {name}</p>}
-				<select type="dropdown" name="dropdownOption" onSubmit={handleChange} onChange={e => setDropdown(e.target.value)}  >
-					<option name="dropdown" dropdown="1" value="1">1</option>
-					<option name="dropdown" dropdown="2" value="2">2</option>
-					<option name="dropdown" dropdown="3" value="3">3</option>
-					<option name="dropdown" dropdown="4" value="4">4</option>
-				</select >
-				{setDropdown !== "" &&  <p>You chose {dropdown}</p>}
-
-				<label>
-					Checkbox: <input type="checkbox" name="myCheckbox" check="checked" onSubmit={handleChange} onChange={e => setChecked(e.target.value)} />
-					{setChecked !== "" &&  <p>You chose {check}</p>}
-				</label>
-				<hr />
-
-
-				<p>
-					Radio buttons:
+		<div className="formwrapper">
+			<div className={toggle}>
+				<form method="post" onSubmit={handleChange}>
 					<label>
+						Enter your name:
 						<input
-						type="radio"
-						name="myRadio"
-						value="option1"
-						onSubmit={handleChange}
-						onChange={e => setRadio(e.target.value)}
-						/>
-						Option 1
-					</label>
-					<label>
-						<input
-							type="radio"
-							name="myRadio"
-							value="option2"
-							onSubmit={handleChange}
-							onChange={e => setRadio(e.target.value)}
-						/>
-						Option 2
-					</label>
-					<label>
-						<input
-						type="radio"
-						name="myRadio"
-						value="option3"
-						onSubmit={handleChange}
-						onChange={e => setRadio(e.target.value)}
-						/>
-						Option 3
-					</label>
+							value={name}
+							name="username"
 
-				</p>
-				{setRadio !== "" &&  <p>You chose {value}</p>}
-				<hr />
-				<button type="reset">Reset form</button>
-				<button type="submit">Submit form</button>
-			</form>
+							onChange={e => setName(e.target.value)}
+						/>
+					</label>
+					{setName !== "" && <p>Your name is {name}</p>}
+					<select type="dropdown" name="dropdownOption" onSubmit={handleChange} onChange={e => setDropdown(e.target.value)}  >
+						<option name="dropdown" dropdown="1" value="1">1</option>
+						<option name="dropdown" dropdown="2" value="2">2</option>
+						<option name="dropdown" dropdown="3" value="3">3</option>
+						<option name="dropdown" dropdown="4" value="4">4</option>
+					</select >
+					{setDropdown !== "" && <p>You chose {dropdown}</p>}
 
+					<label>
+						Checkbox: <input type="checkbox" name="myCheckbox" check="checked" onSubmit={handleChange} onChange={e => setChecked(e.target.value)} />
+						{setChecked !== "" && <p>You chose {check}</p>}
+					</label>
+					<hr />
+					<p>
+						Radio buttons:
+						<label>
+							<input
+								type="radio"
+								name="myRadio"
+								value="option1"
+								onSubmit={handleChange}
+								onChange={e => setRadio(e.target.value)}
+							/>
+							Option 1
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="myRadio"
+								value="option2"
+								onSubmit={handleChange}
+								onChange={e => setRadio(e.target.value)}
+							/>
+							Option 2
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="myRadio"
+								value="option3"
+								onSubmit={handleChange}
+								onChange={e => setRadio(e.target.value)}
+							/>
+							Option 3
+						</label>
+
+					</p>
+					{setRadio !== "" && <p>You chose {value}</p>}
+					<hr />
+
+
+					<button type="submit" onClick={toggleVisible}>Submit form</button>
+				</form>
+			</div>
+			<button type="reset" onClick={refreshPage} >Reset form</button>
 		</div >
 
 	)
-
-
 };
 
 
