f03b4552192657775eb8579a7962ff578a083494
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-10 20:34:48 +0100

    add [function] checkboxType

--- src/Components/PageSection/InputSection.jsx
+++ src/Components/PageSection/InputSection.jsx
@@ -2,13 +2,28 @@ import { DropdownType } from "./InputTypes/DropdownType"
 import { ColorType } from "./InputTypes/ColorType"
 import { RangeType } from "./InputTypes/RangeType"
 import { RadioType } from "./InputTypes/RadioType"
+import { CheckboxType } from "./InputTypes/CheckboxType"
+import { useState } from "react"
 
 export const InputSection = ({question, index, handleOptionSummary}) =>{
+
+const [featureArr, setFeatureArr] = useState([]);
 
 const handleDropDownChange = (value)=>{
     console.log('Index:', index); 
     console.log('Selected Option:', value); 
     handleOptionSummary(value);
+}
+
+const handleChecked = (value)=>{
+    const updatedFeatureArr = [...featureArr, value];
+    setFeatureArr(updatedFeatureArr);
+    handleOptionSummary(updatedFeatureArr);
+}
+
+const handleUncheck = (value)=>{
+    const updatedFeatureArr = featureArr.filter((item) => item !== value);
+    handleOptionSummary(updatedFeatureArr);
 }
 
 const handleChange = (event)=>{
@@ -26,11 +41,11 @@ const handleChange = (event)=>{
             }
 
             { index === 1 && 
-                <ColorType question={question} handleChange={handleChange}/>
+                <ColorType question={question} handleChange={handleChange} />
             }
             
             { index === 2 && 
-                <RadioType question={question} handleChange={handleChange}/>
+                <CheckboxType question={question} handleChecked={handleChecked} handleUncheck={handleUncheck}handleChange={handleChange}/>
             }
 
             { index === 3 && 
