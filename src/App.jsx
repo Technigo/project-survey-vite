9b8a6552143022db551482451ff047aa8c7dfc00
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-11 21:02:06 +0100

    Add form and label and made the input accessible

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ export const App = () => {
     <header>
       <h1>What's the best you?</h1>
     </header>
+    <form>
+      <div>
+        {/* "for" and "id" has to match, it's for accessabiliy  */}
+        <label for="bestThing">What's the best thing about you?</label>
+        <input type="text" id="bestThing" name="bestThing" />
+      </div>
+    
+      
+    </form>
     </div>
 
 
