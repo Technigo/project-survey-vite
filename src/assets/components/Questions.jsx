f66792b437e79f46adfbecd13b32198aed0047fa
Citronskal <yiaswift@gmail.com>
2024-03-12 22:06:21 +0100

    radio buttons

--- src/assets/components/Questions.jsx
+++ src/assets/components/Questions.jsx
@@ -4,7 +4,7 @@ function Questions() {
   const [activity, setActivity] = useState("");
   const [music, setHappy] = useState("");
   const [songs, setSelectSong] = useState("");
-  const [allQuestionsAnswered, setAllQuestionsAnswered] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   function handleActivityChange(event) {
     setActivity(event.target.value);
@@ -18,9 +18,10 @@ function Questions() {
     setSelectSong(event.target.value);
   }
 
-  function checkAllQuestionsAnswered() {
+  function handleSubmit(event) {
+    event.preventDefault();
     if (activity !== "" && music !== "" && songs !== "") {
-      setAllQuestionsAnswered(true);
+      setSubmitted(true);
     } else {
       alert("Please answer all questions.");
     }
@@ -28,49 +29,45 @@ function Questions() {
 
   return (
     <div>
-      <h2>1.</h2>
-      <p>What is your mood-boosting activity?</p>
-      <textarea value={activity} onChange={handleActivityChange} />
-
-      <h2>2.</h2>
-      <p>Does music make you happy?</p>
-      <button
-        style={{
-          backgroundColor: music === "Yes" ? "darkgray" : "",
-          color: music === "Yes" ? "white" : "black",
-        }}
-        onClick={() => toggleMusicHappy("Yes")}
-      >
-        Yes, absolutely
-      </button>
-      <button
-        style={{
-          backgroundColor: music === "No" ? "darkgray" : "",
-          color: music === "No" ? "white" : "black",
-        }}
-        onClick={() => toggleMusicHappy("No")}
-      >
-        No, I like the silence
-      </button>
-      <h2>3.</h2>
-      <p>What is your go-to happy song?</p>
-      <select value={songs} onChange={selectHappySongs}>
-        <option value="">Select songs</option>
-        <option>Ariana Grande - yea, and?</option>
-        <option>SZA - Saturn</option>
-        <option>Dua Lipa - Training Session</option>
-        <option>Taylor Swift - Cruel Summer</option>
-      </select>
-      <br />
-
-      <button onClick={checkAllQuestionsAnswered}>Submit</button>
-
-      {allQuestionsAnswered && (
+      {!submitted ? (
+        <form onSubmit={handleSubmit}>
+          <h2>1.</h2>
+          <p>What is your mood-boosting activity?</p>
+          <textarea value={activity} onChange={handleActivityChange} />
+          <h2>2.</h2>
+          <p>Does music make you happy?</p>
+          <input
+            type="radio"
+            name="music"
+            value="Yes"
+            onClick={() => toggleMusicHappy("Yes")}
+          />
+          Yes, absolutely
+          <input
+            type="radio"
+            name="music"
+            value="No"
+            onClick={() => toggleMusicHappy("No")}
+          />
+          No, I like the silence
+          <h2>3.</h2>
+          <p>What is your go-to happy song?</p>
+          <select value={songs} onChange={selectHappySongs}>
+            <option value="">Select songs</option>
+            <option>Ariana Grande - yea, and?</option>
+            <option>SZA - Saturn</option>
+            <option>Dua Lipa - Training Session</option>
+            <option>Taylor Swift - Cruel Summer</option>
+          </select>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
         <div>
-          <h2>Your Answers:</h2>
-          <p>Mood-boosting activity: {activity}</p>
-          <p>Does music make you happy?: {music}</p>
-          <p>Go-to happy song: {songs}</p>
+          <h2>Thank you for answering!</h2>
+          <p>{activity} is your mood-boosting activity</p>
+          <p>{music} is making you happy,</p>
+          <p>and if ever you're down - listen to {songs}</p>
         </div>
       )}
     </div>
