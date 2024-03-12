eb402dc92c959e76971b7c0db39fd5d5eb616559
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-12 20:10:27 +0100

    Add htmlfor and comment about it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // here we import the useState from react, so we can use it to update the mode to "summary" when the form is submitted.
 // https://legacy.reactjs.org/docs/hooks-state.html
-import React, { useState } from 'react';
+import React, { useState } from "react";
 export const App = () => {
   // here we created a varible. so we can update mode  to "summary" when the form is submitted.
   const [mode, setMode] = useState("form");
@@ -18,22 +18,22 @@ export const App = () => {
       </header>
       {/* Similar to "if statement, called conditional rendering" https://legacy.reactjs.org/docs/conditional-rendering.html */}
       {mode == "form" && (
-   
-      <form onSubmit={onSubmit}>      {/* here we register the function (event handler) */}
-
-        <div>
-          {/* "for" and "id" has to match, it's for accessabiliy  */}
-          <label for="bestThing">What's the best thing about you?</label>
-          <input type="text" id="bestThing" name="bestThing" />
-        </div>
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-      // This is the end of the form/conditional rendering
+        <form onSubmit={onSubmit}>
+          {" "}
+          {/* here we register the function (event handler) */}
+          <div>
+            {/* "for" and "id" has to match, it's for accessabiliy  */}
+            {/* use "htmlfor", otherwise you will get a warning in the console log.  */}
+            <label htmlFor="bestThing">What's the best thing about you?</label>
+            <input type="text" id="bestThing" name="bestThing" />
+          </div>
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+        // This is the end of the form/conditional rendering
       )}
       {mode == "summary" && <div>summary</div>}
-
     </div>
   );
 };
