85e4afddb92c000bf66ab03163c0d9f2b2b85604
Lovisaaberg <155725277+Lovisaaberg@users.noreply.github.com>
2024-03-13 13:57:56 +0100

    Added useState to question 3 (#2)

--- src/components/questions/QuestionThree.jsx
+++ src/components/questions/QuestionThree.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
 import "./Questions.css"
 
+
 export const QuestionThree = () => {
+  const [song, setSong] = useState("");
+
   return (
-    <div>This one is supposed to have select option</div>
+    <div className="3">
+      <span>3.</span>
+      <h3>What is your go-to happy song?</h3>
+      <select name="questionThree"
+        onChange={event => setSong(event.target.value)}
+        value={song}
+        required
+        >
+        <option value="">Select</option>
+        <option value="song1">First option</option>
+        <option value="song2">Second option</option>
+        <option value="song3">Third option</option>
+      </select>
+    </div>
   )
 }
