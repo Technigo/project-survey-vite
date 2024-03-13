29057c88948be88a08d68598096d219c6ab53db4
Pernilla Sterner <hey@pernillasterner.se>
2024-03-13 13:54:59 +0100

    Add first question and create a function that handles input value (#1)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,18 @@
-import "./Form.css"
+import "./Form.css";
 import { QuestionOne } from "./questions/QuestionOne";
 import { QuestionTwo } from "./questions/QuestionTwo";
 import { QuestionThree } from "./questions/QuestionThree";
-
-
+import { useState } from "react";
 
 export const Form = () => {
+  // Save anwers
+  const [answers, setAnswers] = useState({});
+
   return (
     <form>
       <QuestionOne />
       <QuestionTwo />
       <QuestionThree />
     </form>
-  )
-}
+  );
+};
--- src/components/questions/QuestionOne.jsx
+++ src/components/questions/QuestionOne.jsx
@@ -1,7 +1,33 @@
-import "./Questions.css"
+import { useState } from "react";
+import "./Questions.css";
 
 export const QuestionOne = () => {
+  const [value, setValue] = useState("");
+  const [error, setErrorMessage] = useState("");
+
+  const handleValue = (inputValue) => {
+    // Handle error message
+    console.log(inputValue);
+
+    if (!inputValue) {
+      setErrorMessage("Sorry but you need to add an activity");
+    }
+  };
+
   return (
-    <div>This is the firt question. It should be an input field</div>
-  )
-}
+    <div className="question-container">
+      <span className="count">1.</span>
+      <h3>What is your favorite mood-boosting activity?</h3>
+      <label>
+        <input
+          type="text"
+          name="questionOne"
+          placeholder="Write your answer here..."
+          onClick={() => handleValue(value)}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
+      </label>
+    </div>
+  );
+};
