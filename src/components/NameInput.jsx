367e14bef9e2ca023bdd1957003622a6a1b7e6cb
schouenkes <m.schouenke@gmail.com>
2024-03-13 18:40:33 +0100

    added onFocus & onBlur to text input for the placeholder

--- src/components/NameInput.jsx
+++ src/components/NameInput.jsx
@@ -1,20 +1,9 @@
 import { useState } from "react";
+import { HandleFocus } from "./HandleFocus.jsx";
 
 export const NameInput = () => {
   const [name, setName] = useState("");
-  const [placeholder, setPlaceholder] = useState("Type your full name");
-
-  // when the input field is active the placeholder disappear
-  const handleFocus = () => {
-    setPlaceholder("");
-  };
-
-  // if the input field is empty when focusing on another input, the placeholder will appear.
-  const handleOnBlur = () => {
-    if (name === "") {
-      setPlaceholder("Type your full name");
-    }
-  };
+  const { placeholder, handleFocus, handleOnBlur } = HandleFocus("Type your full name");
 
   return (
     <label className="text-form-area">
@@ -24,7 +13,7 @@ export const NameInput = () => {
         placeholder={placeholder}
         onChange={(event) => setName(event.target.value)}
         onFocus={handleFocus}
-        onBlur={handleOnBlur}
+        onBlur={() => handleOnBlur(name)}
         value={name}
       />
     </label>
