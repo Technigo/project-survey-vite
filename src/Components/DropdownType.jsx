ff40797f5389d5d4f055612418deb7b714af1db1
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-13 20:54:18 +0100

    update [function] add option display in DropdownType

--- src/Components/DropdownType.jsx
+++ src/Components/DropdownType.jsx
@@ -3,25 +3,39 @@ import { useState } from "react"
 export const DropdownType = ({question, handleDropDownChange}) => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleOptionClick = (option) =>{
+        setSelectedOption(option);
+        handleDropDownChange(option);
+        setIsOpen(false);
+    }
+
+    const handleOpenOptions = ()=>{
+        setIsOpen(!isOpen);
+    }
 
     return (
         <section className={`input-section ${question.id}`}>
             <p>{question.questionText}</p>
-                <button type="button" onClick={()=>setIsOpen(!isOpen)}>Selec your dream home</button>
-                {isOpen && (
-                    <ul>
-                        {question.options.map((option)=>(
-                            <li 
-                                key={option.id} 
-                                value={option.optionText} 
-                                onClick={()=>handleDropDownChange(option.optionText)}>
-                                {option.optionText}
-                            </li>
+            <button type="button" onClick={handleOpenOptions}>
+                Select your dream home
+            </button>
+            {isOpen && (
+                <ul>
+                    {question.options.map((option)=>(
+                        <li 
+                            key={option.id} 
+                            value={option.optionText} 
+                            onClick={()=>handleOptionClick(option.optionText)}>
+                             {option.optionText}
+                        </li>
                         ))}
-                    </ul>
-                )}
-
-               
+                </ul>
+            )}
+            {selectedOption && (
+            <p>You choose: <span>{selectedOption}</span></p>
+            )}
         </section>
     )
 }
