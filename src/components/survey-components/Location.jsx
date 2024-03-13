78e54aa52a8f51eff297e2a28cfe2aa025b027b1
Martin Jönsson <martin.t.joensson@gmail.com>
2024-03-13 09:30:44 +0100

    Finished up the location selector

--- src/components/survey-components/Location.jsx
+++ src/components/survey-components/Location.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 export const Location = () => {
+  const [selectedLocation, setSelectedLocation] = useState("");
+
   return (
-    <div className="location">
-      <p>Location component</p>
+    <div className="question location">
+      <h2>3. Where do you usually eat pizza?</h2>
+      <select
+        value={selectedLocation}
+        defaultValue={"default"}
+        onChange={(event) => setSelectedLocation(event.target.value)}
+      >
+        <option value="default" default disabled>
+          -- Please Choose Continent --
+        </option>
+        <option value="Africa">Africa</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="South America">South America</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+      <p>Your favorite pizza-eating-spot is: {selectedLocation}</p>
     </div>
   );
 };
