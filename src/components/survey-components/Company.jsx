f7fa054f9eeeb741b034beb6c21ace81d27da75a
izzibizz <contact@izabellind.com>
2024-03-13 20:14:05 +0100

    changed input to dropdown and added images of the celebrities

--- src/components/survey-components/Company.jsx
+++ src/components/survey-components/Company.jsx
@@ -10,12 +10,19 @@ const selectCompany = (event) => {
 return(
 <div className="question company">
 <h3>4. Who do you dream of eating pizza with?</h3>
-      <form>
-        <input 
-        type="text"
-        value={company}
-        onChange={selectCompany}></input>
-      </form>
+<select value={company} onChange={selectCompany}>
+      <option value="" selected="selected" disabled hidden>
+        Company of your choice
+        </option>
+        <option value="Michelle Obama">Michelle Obama</option>
+        <option value="Albert Einstein">Albert Einstein</option>
+        <option value="Jackie Chan">Jackie Chan</option>
+        <option value="Margaret Thatcher">Margaret Thatcher</option>
+        <option value="Martin Luther King Jr">Martin Luther King Jr</option>
+        <option value="Frida Kahlo">Frida Kahlo</option>
+        <option value="Vladimir Putin">Vladimir Putin</option>
+        <option value="Yayoi Kusama">Yayoi Kusama</option>
+      </select>
       <p><span>We will organize a pizza date for you and {company}</span></p>
       </div>
 )
