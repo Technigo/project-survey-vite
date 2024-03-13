d8c6af43aef623ee6c6736543b3719d79ca85ee5
izzibizz <contact@izabellind.com>
2024-03-13 13:25:27 +0100

    added some styles to dropdown etc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import headerImage from "../assets/header1.jpg"
-import "./header.css"
+import headerImage from '../assets/header1.jpg'
+import './header.css'
 
 export const Header = () => {
-    return (
-        <header>
-        <h1>Pizza Survey</h1>
-        <img src={headerImage} id="headerImg" alt="Image of flying pizza in the sky"></img></header>
-    )
+  return (
+    <header>
+      <h1>
+        <span>Pizza Survey</span>
+      </h1>
+      <img
+        src={headerImage}
+        id="headerImg"
+        alt="Image of flying pizza in the sky"
+      ></img>
+    </header>
+  )
 }
