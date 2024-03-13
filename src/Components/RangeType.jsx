c2b8b6aaa0d438b8104487856acf203cd2ea39fb
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-13 21:32:09 +0100

    update colorType

--- src/Components/RangeType.jsx
+++ src/Components/RangeType.jsx
@@ -2,16 +2,25 @@ export const RangeType = ({ question, handleChange }) => {
   return (
     <section className={`input-section ${question.id}`}>
       <label htmlFor={question.id}>{question.questionText}</label>
-      <p>Not important at all</p>
+
+      <p>Not necessary</p>
+
       <input
         type="range"
         id={question.id}
         name={question.id}
         min="0"
         max="10"
+        list="{question.id}"
         onChange={handleChange}
       />
-      <p>Very important</p>
+      <datalist id="{question.id}">
+        <option value="0">Not necessary</option>
+        <option value="5">Somewhat important</option>
+        <option value="10">Extremely important</option>
+      </datalist>
+
+      <p>Extremely important</p>
     </section>
   );
 };
