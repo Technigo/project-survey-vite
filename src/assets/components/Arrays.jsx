cdbd807ede334deeb45bebcd7eaab41d0400fe7b
Citronskal <yiaswift@gmail.com>
2024-03-14 19:55:11 +0100

    First and second questions done

---
+++ src/assets/components/Arrays.jsx
@@ -0,0 +1,47 @@
+// creates a companions array with name and description
+export const companionsArray = [
+  {
+    name: "Puddle the Pudding Pixie",
+    description:
+      "A tiny pixie who lives in a jar of pudding on the bedside table. Loves to make bedtime snacks extra special by adding a sprinkle of magic.",
+  },
+  {
+    name: "Buzzy the Bumblebee",
+    description:
+      "A chubby, friendly bumblebee with wings that flutter softly as it buzzes around the room. A great storyteller and adds a touch of magic to bedtime.",
+  },
+  {
+    name: "Sage the Talking Squirrel",
+    description:
+      "A wise and witty squirrel who lives in the old oak tree just outside the bedroom window. Loves to share stories of far-off lands and fantastical adventures.",
+  },
+  {
+    name: "Twinkle the Tooth Fairy",
+    description:
+      "A magical and graceful fairy who visits children's bedrooms at night to collect their lost teeth. Leaves behind a sprinkle of fairy dust and a shiny coin in exchange for each tooth.",
+  },
+];
+
+// creates an adventures array with name and description
+export const adventuresArray = [
+  {
+    name: "Magical Treasure Hunt",
+    description:
+      "Embark on a magical journey through the enchanted forest in search of hidden treasures left behind by mystical creatures. Follow whimsical clues and let your imagination soar.",
+  },
+  {
+    name: "Bedtime Concert",
+    description:
+      "Set up a cozy stage area and showcase your talents in a magical bedtime concert. Sing enchanting melodies, tell jokes, and perform dazzling feats of magic before drifting off to sleep.",
+  },
+  {
+    name: "Dreamy Drawings",
+    description:
+      "Engage in a creative art project using various art supplies to express yourself and explore your imagination. Let your artwork transport you to a world of wonder and whimsy.",
+  },
+  {
+    name: "Stargazing",
+    description:
+      "Set up a cozy spot by the window or outdoors and observe the wonders of the night sky. Gaze at the stars, identify constellations, and share stories of the cosmos as you drift off into dreams.",
+  },
+];
--- src/assets/components/Questions.jsx
+++ src/assets/components/Questions.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
+import { companionsArray, adventuresArray } from "./Arrays.jsx";
+
 
 function Questions() {
-  const [activity, setActivity] = useState("");
-  const [music, setHappy] = useState("");
-  const [songs, setSelectSong] = useState("");
+  const [charactername, setCharactername] = useState("");
+  const [location, setLocation] = useState("");
+  const [companion, setCompanion] = useState("");
+  const [adventure, setAdventure] = useState("");
+
   const [submitted, setSubmitted] = useState(false);
 
-  function handleActivityChange(event) {
-    setActivity(event.target.value);
+  function characterName(event) {
+    setCharactername(event.target.value);
   }
 
-  const toggleMusicHappy = (value) => {
-    setHappy(value);
+  const chooseLocation = (value) => {
+    setLocation(value);
   };
 
-  function selectHappySongs(event) {
-    setSelectSong(event.target.value);
+  function selectCompanion(event) {
+    setCompanion(event.target.value);
+  }
+
+  //added function for adventures
+  function selectAdventure(event) {
+    setAdventure(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (activity !== "" && music !== "" && songs !== "") {
+    if (
+      charactername !== "" &&
+      music !== "" &&
+      companion !== "" &&
+      adventure !== ""
+    ) {
       setSubmitted(true);
     } else {
       alert("Please answer all questions.");
@@ -33,56 +47,97 @@ function Questions() {
         <form onSubmit={handleSubmit} className="QuestionForm">
           <div className="FirstQuestion">
             <h2 className="number">1.</h2>
-            <p className="question">What is your mood-boosting activity?</p>
+            <p className="question">What is your character's name?</p>
             <textarea
-              value={activity}
-              onChange={handleActivityChange}
-              className="activity"
+              value={charactername}
+              onChange={characterName}
+              className="Charactername"
             />
           </div>
           <div className="SecondQuestion">
             <h2 className="number">2.</h2>
-            <p className="question">Does music make you happy?</p>
+            <p className="question">
+              Choose where you want to start your story?
+            </p>
             <div className="RadioButtons">
               <input
                 type="radio"
                 name="music"
-                value="Yes"
-                onClick={() => toggleMusicHappy("Yes")}
-                className="yes-radio"
+                value="Forest"
+                onClick={() => chooseLocation("Forest")}
+                className="radio"
               />
-              <label>Yes, absolutely</label>
+              <label>Forest</label>
               <input
                 type="radio"
                 name="music"
-                value="No"
-                onClick={() => toggleMusicHappy("No")}
-                className="no-radio"
+                value="A far far away kingdom"
+                onClick={() => chooseLocation("A far far away kingdom")}
+                className="radio"
               />
-              <label>No, I like the silence</label>
+              <label>A far far away kingdom</label>
+              <input
+                type="radio"
+                name="music"
+                value="School of Witchcraft and Wizardry"
+                onClick={() =>
+                  chooseLocation("School of Witchcraft and Wizardry")
+                }
+                className="radio"
+              />
+              <label>School of Witchcraft and Wizardry</label>
             </div>
           </div>
 
           <div className="ThirdQuestion">
             <h2 className="number">3.</h2>
-            <p className="question">What is your go-to happy song?</p>
-            <select value={songs} onChange={selectHappySongs}>
-              <option value="">Select songs</option>
-              <option>Ariana Grande - yea, and?</option>
-              <option>SZA - Saturn</option>
-              <option>Dua Lipa - Training Session</option>
-              <option>Taylor Swift - Cruel Summer</option>
+            <p className="question">Who is your main character's companion?</p>
+            <select value={companion} onChange={selectCompanion}>
+              <option value="">Select companion</option>
+              {companionsArray.map((companion, index) => (
+                <option key={index} value={companion.name}>
+                  {companion.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="FourthQuestion">
+            <h2 className="number">4.</h2>
+            <p className="question">What's the pair's favourite adventure?</p>
+            <select value={adventure} onChange={selectAdventure}>
+              <option value="">Select adventure</option>
+              {adventuresArray.map((adventure, index) => (
+                <option key={index} value={adventure.name}>
+                  {adventure.name}
+                </option>
+              ))}
             </select>
             <br />
-            <button type="submit">Submit</button>
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
         </form>
       ) : (
         <div>
-          <h2>Thank you for answering!</h2>
-          <p>{activity} is your mood-boosting activity</p>
-          <p>{music} is making you happy,</p>
-          <p>and if ever you're down - listen to {songs}</p>
+          <h2>Thank you for answering! Here's your whimsical bedtime story</h2>
+          <p>Your name is {charactername}, and you are tasked with an important mission and about start your adventure!</p>
+          <p>You are excited to leave {location} and make some new friends!</p>
+          <p>
+            {
+              companionsArray.find(
+                (companionElement) => companionElement.name === companion
+              ).description
+            }
+          </p>
+          <p>
+            {
+              adventuresArray.find(
+                (adventureElement) => adventureElement.name === adventure
+              ).description
+            }
+          </p>
         </div>
       )}
     </div>
