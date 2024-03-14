c192d1253cdef71a9c6bbcb2a2b52b1b5a26a7e1
idsab <idasabic@gmail.com>
2024-03-14 22:14:34 +0100

    working on h1,h2,p structure for accessibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
     return (
       <div className="header-container">
         <h1>Exercise Survey 🤸‍♀️</h1>
-        <h3>We want to hear about your exercise habits! Please share by answering our form below. </h3>
+        <p>We want to hear about your exercise habits! Please share by answering our form below. </p>
       </div>
     )
   }
