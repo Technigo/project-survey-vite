6000ebf9a4dd2342f478c31ff619e26830caeaf7
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-03-14 12:25:52 +0100

    added github icons in files

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
+import gitIcon from './icons8-github.svg'
+
 export const Footer = () => {
   return (
     <div className="footer">
       <section className="copyright">©VittoriaMatteoli</section>
       <section className="links">
-        <a
-          href="https://github.com/vittoriamatteoli"
-          // className="fa-brands fa-github"
-        >
-          GitHub
+        <a href="https://github.com/vittoriamatteoli">
+          {' '}
+          <img src={gitIcon} alt="gitHub-icon" />
         </a>
       </section>
     </div>
