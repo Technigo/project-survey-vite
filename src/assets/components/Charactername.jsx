1316101687676a6d4b57901bd5b143b33e3021d5
Your El1an3 <eliane_w@yahoo.com>
2024-03-15 18:47:36 +0100

    fixed accessibility

--- src/assets/components/Charactername.jsx
+++ src/assets/components/Charactername.jsx
@@ -1,13 +1,18 @@
 // eslint-disable-next-line react/prop-types
 export function Charactername({ value, onChange }) {
   return (
-    <div>
-      <h2 className="question">What is your character&#39;s name?</h2>
-      <textarea
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        className="Charactername"
-      />
+    <div className="elementContainer">
+      <label>
+        <h2 className="question">What is your character&#39;s name?</h2>
+        <input
+          aria-describedby="characterName"
+          name="characterName"
+          id="characterName"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          // className="Charactername"
+        />
+      </label>
     </div>
   );
 }
