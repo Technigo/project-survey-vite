97cd2d083e78108732aa7b47ad3089add562d1b0
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-03-16 12:00:04 +0100

    added ' with &apos

--- src/assets/Components/Result/Result.jsx
+++ src/assets/Components/Result/Result.jsx
@@ -4,7 +4,9 @@ export const Result = ({ coffee, cups, reason, time, rate }) => {
       <p>{coffee} is your favourite Coffee ☕</p>
       <p>You drink {cups} of coffee per day!</p>
       <p>You drink coffee for {reason} </p>
-      <p>{time} it`s your preferred time slot to enjoy a coffee!</p>
+      <p>
+        {time} it<> &apos; </>s your preferred time slot to enjoy a coffee!
+      </p>
       <p>Coffee holds an {rate}% significance in your daily life routine.</p>
       <h3>Thank you for taking part of this survey!</h3>
     </div>
