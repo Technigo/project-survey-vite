8bdd038a91cb47d8956c18c8d356474c23f7eae2
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-03-17 18:37:30 +0100

    Update StartPage.jsx

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -7,7 +7,7 @@ import PropTypes from "prop-types";
 export const StartPage = ({ onStartSurvey }) => {
   return (
     <div className="start-page">
-      <img src={startPageImage} alt="Music Survey Image" />
+      <img src={startPageImage} alt="Music Survey Image" loading="lazy" />
       <div className="text-container">
         <h1>Music Survey</h1>
         <button onClick={onStartSurvey}>Let&apos;s start!</button>
