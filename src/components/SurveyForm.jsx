36083a953d5fd52696899c272c58032663b2ff17
loren <lorenzo.dastoli@gmail.com>
2024-03-20 10:50:15 +0100

    added gender + rate section

--- src/components/SurveyForm.jsx
+++ src/components/SurveyForm.jsx
@@ -22,6 +22,10 @@ const SurveyForm = () => {
 
   return (
     <div className="container">
+      <div className='header'>
+        <h1>Technigo Bootcamp</h1>
+        <h2>Evaluate the course</h2>
+      </div>
       {!submitted ? (
         <form onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label>
@@ -43,14 +47,61 @@ const SurveyForm = () => {
             onChange={handleChange}
             required
           />
+          <label htmlFor="gender">Gender</label>
+          <select id="gender" name="gender" value={formData.gender} onChange={handleChange} required>
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="non-binary">Non-binary</option>
+            <option value="genderqueer">Genderqueer</option>
+            <option value="genderfluid">Genderfluid</option>
+            <option value="agender">Agender</option>
+            <option value="bigender">Bigender</option>
+            <option value="androgynous">Androgynous</option>
+            <option value="two-spirit">Two-Spirit</option>
+            <option value="demiboy">Demiboy</option>
+            <option value="demigirl">Demigirl</option>
+            <option value="gender-nonconforming">Gender Nonconforming</option>
+            <option value="pangender">Pangender</option>
+            <option value="neutrois">Neutrois</option>
+            <option value="gender-questioning">Gender Questioning</option>
+            <option value="gender-variant">Gender Variant</option>
+            <option value="transgender">Transgender</option>
+            <option value="cisgender">Cisgender</option>
+            <option value="intergender">Intergender</option>
+            <option value="polygender">Polygender</option>
+            <option value="other"></option>
+          </select>
+
+          <label>How would you rate your experience at Technigo bootcamp?</label><br/>
+          <input type="radio" id="very_satisfied" name="rate" value="very satisfied" onChange={handleChange} />
+          <label htmlFor="very_satisfied">Very Satisfied</label><br/>
+
+          <input type="radio" id="satisfied" name="rate" value="satisfied" onChange={handleChange} />
+          <label htmlFor="satisfied">Satisfied</label><br/>
+
+          <input type="radio" id="neither_agree_nor_disagree" name="rate" value="Neither agree nor disagree" onChange={handleChange} />
+          <label htmlFor="neither_agree_nor_disagree">Neither Agree nor Disagree</label><br/>
+
+          <input type="radio" id="dissatisfied" name="rate" value="dissatisfied" onChange={handleChange} />
+          <label htmlFor="dissatisfied">Dissatisfied</label><br/>
+
+          <input type="radio" id="very_dissatisfied" name="rate" value="very dissatisfied" onChange={handleChange} />
+          <label htmlFor="very_dissatisfied">Very Dissatisfied</label><br/>
 
           <button type="submit">Submit</button>
         </form>
       ) : (
-        <div>Form Submitted</div>
+        <div className='summary'>
+          <h2>Thank you for participating!</h2>
+          <p><strong>Name:</strong> {formData.name}</p>
+          <p><strong>Age:</strong> {formData.age}</p>
+          <p><strong>Gender:</strong> {formData.gender}</p>
+          <p><strong>Rate:</strong> {formData.rate}</p>
+        </div>
       )}
     </div>
   );
-};
+}
 
 export default SurveyForm;
