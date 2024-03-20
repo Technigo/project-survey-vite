9de4bb70473ce6c313b4cb59af0b1a74a09fe949
Katarina Sjölin <kat.sjolin@gmail.com>
2024-03-20 11:25:15 +0100

    added radio buttons and select dropdown

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-export const Form = ({ name, setName, age, setAge, setShowSummary }) => {
+export const Form = ({ name, setName, age, setAge, setShowSummary}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -9,12 +9,30 @@ export const Form = ({ name, setName, age, setAge, setShowSummary }) => {
       <form onSubmit={handleSubmit}>
           <div>
              <label htmlFor="nameInput">name:</label>
-             <input id="nameInput" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+             <input id="nameInput" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} required></input>
           </div>
 
           <div>
              <label htmlFor="ageInput">age:</label>
-             <input id="ageInput" name="age" type="number" value={age} onChange={(e) => setAge(e.target.value)}></input>
+             <input id="ageInput" name="age" type="number" value={age} onChange={(e) => setAge(e.target.value)} required></input>
+          </div>
+
+          <div>
+             <p>What is the best thing about Easter?</p>
+             <label><input type='radio' name="best_thing" value="Church"></input>Visiting church</label>
+             <label><input type='radio' name="best_thing" value="EggHunt"></input>Go egg hunting</label>
+             <label><input type='radio' name="best_thing" value="PaintEggs"></input>To paint eggs</label>
+             <label><input type='radio' name="best_thing" value="Eating"></input>Eating delicious Easter food</label>
+          </div>
+
+          <div>
+             <p>What is your favourite holiday?</p>
+             <select required>
+               <option value="">Select favourite holiday</option>
+               <option value="christmas">Christmas</option>
+               <option value="easter">Easter</option>
+               <option value="midsummer">Midsummer</option>
+             </select>
           </div>
 
           <button type="submit">Submit form</button>
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -6,12 +6,13 @@ export const Summary = ({ name, setName, age, setAge, setShowSummary }) => {
     setShowSummary(false)
   }
 
-  
   return (
     <div>
-      <h2>Thank you for doing our Happy Easter Survey!</h2>
+      <h2>Thank you for doing our Easter Survey!</h2>
+      <h3>Here is the summary of your answers:</h3>
       <p>Your name is {name} and you are {age} years old.</p>
-      <button onClick={clearForm}>Restart</button>
+      <p>The best thing about Easter is and your favourite holiday is.</p>
+      <button onClick={clearForm}>Restart survey</button>
     </div>
   )
 }
